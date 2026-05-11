c071d459c84a3622f74cfa169d6bc06e63af8ae6
rhkasbohm <rkasbohm@uw.edu>
2026-05-11 14:51:38 -0700

    Add files via upload

---
+++ CrabLab.R
@@ -0,0 +1,125 @@
+install.packages("tidyverse")
+install.packages("paletteer")
+install.packages("ggpubr")
+install.packages("lmboot")
+library(tidyverse)
+library(paletteer)
+library(ggpubr)
+library(lmboot)
+
+#Bar charts for baselines. Easiest graph to do if we keep the datasheet wide,
+#rather than keeping it long. This way we also do not have to account for weight!
+
+Baseline <- read.csv(file = "Baseline Data.csv", header = T)
+
+sum.baseline <- Baseline %>%
+  group_by(Genus, temp) %>%
+  summarise(
+    N = n(),
+    mean = mean(Percent.change),
+    sd = sd(Percent.change),
+    se = sd / sqrt(N),
+  )
+
+
+
+
+ggplot(data=sum.baseline, aes(x = Genus, y = mean, fill = Genus))+
+  geom_col(position="dodge", width = 0.5)+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.1, position= position_dodge((0.5))) +
+  scale_fill_manual(values = c("Blank" = "grey85", "Carcinus"="turquoise4","Hemigrapsus"="darkgoldenrod"))+
+  labs(x=" ", y = "Percent Change", fill = "Genus")+
+  theme_bw()+
+  theme(legend.position="none", text=element_text(size=16,family="serif"),)
+
+
+#Baseline take TWO !!!! making a line graph. To do this, the datasheet has to be 
+#reconfigured to by long, not wide. It can stay wide if we're doing % change !!
+
+Baseline_line <- read.csv(file = "Baseline.csv", header = T)
+
+sum.baselineII <- Baseline_line %>%
+  group_by(Time, Genus) %>%
+  summarise(
+    N = n(),
+    mean = mean(Fluorescence),
+    sd = sd(Fluorescence),
+    se = sd / sqrt(N),
+  )
+
+ggplot(data=sum.baselineII, aes(x = Time, y = mean, group= Genus, color = Genus))+
+  geom_line()+
+  geom_point(size=1.5)+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.1) +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 0.5) +
+  stat_regline_equation(label.x= c(10),label.y = c(4500, 5500, 6500)) + 
+  theme_minimal()+
+  labs(x = "Time",
+       y= "Fluorescence")+
+  theme(text=element_text(size=12,family="serif"))+
+  scale_color_manual(values = c("Blank" = "grey85", "Carcinus"="turquoise4","Hemigrapsus"="darkgoldenrod"))
+
+
+#Normalized by weight; blanks are divided by the weight of the amount of water, 
+#So either 8 grams or 40 grams, depending on which blank it was
+
+sum.normal.fluor <- Baseline_line %>%
+  group_by(Time, Genus) %>%
+  summarise(
+    N = n(),
+    mean = mean(Normal.Fluor),
+    sd = sd(Normal.Fluor),
+    se = sd / sqrt(N),
+  )
+
+ggplot(data=sum.normal.fluor, aes(x = Time, y = mean, group= Genus, color = Genus))+
+  geom_line()+
+  geom_point(size=1.5)+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.1) +
+  geom_smooth(method = "lm", se = FALSE, linewidth=0.5) +
+  stat_regline_equation(label.x= c(10),label.y = c(2500, 3000, 3500)) + 
+  theme_minimal()+
+  labs(x = "Time",
+       y= "Fluorescence/gram body weight")+
+  theme(text=element_text(size=12,family="serif"))+
+  scale_color_manual(values = c("Blank" = "grey85", "Carcinus"="turquoise4","Hemigrapsus"="darkgoldenrod"))
+ 
+#Running into the issue here that the EGCs are so weighty that they make the 
+#fluor/gram quite low (like ~4 or 5), so if we were to do this, we'd need a 
+#way to convert fluor to O2. 
+
+#OOOOH! or we could just do changes in metabolic rate. So take the slope of each
+#species at each temp, and do the change. So if the hemis here are 56/whatever
+#and in the hot treatment they're like 100/whatever that's like a ~79% increase in 
+#metabolic rate. Much to consider... 
+
+
+#need to bootstrap the green crabs because of difference in sample size.
+#alternatively, we compare means of change in respiration just between each 
+#genus to avoid the need to bootstrap 
+#i no no wanna bootstrap :(
+
+BaselineTest <- read.csv(file = "Baseline_noBLank_test.csv", header = T)
+
+sum.baselineTest <- BaselineTest %>%
+  group_by(Genus) %>%
+  summarise(
+    N = n(),
+    mean = mean(Percent.change),
+    sd = sd(Percent.change),
+    se = sd / sqrt(N),
+  )
+
+t.test(sum.baselineTest$mean)
+
+anovaTest <- aov(mean ~ Genus*temp, data = sum.baseline)
+summary(anovaTest)
+TukeyHSD(anovaTest)
+
+anovaTake2 <- aov(mean ~ as.factor(Genus), data = sum.baseline)
+summary(anovaTake2)
+TukeyHSD(anovaTake2, conf.level = 0.90)
+
+
+
+
